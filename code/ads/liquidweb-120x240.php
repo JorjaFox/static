81b667ba4d25ad68ef922a3580174b008bd1436a
Mika Epstein (Ipstenu) <ipstenu@ipstenu.org>
2013-09-15 10:28:47 -0700

    Remove admin folder

--- code/ads/liquidweb-120x240.php
+++ code/ads/liquidweb-120x240.php
@@ -1,1 +1,1 @@
-<a href="http://www.liquidweb.com/?RID=JFO123"><img src="http://rgfx.liquidweb.com/banners/120x240.jpg" alt="Liquid Web Fully Managed Web Hosting" border=0></a>
+<a href="http://www.liquidweb.com/?RID=JFO123"><img src="http://rgfx.liquidweb.com/banners/120x240.jpg" alt="Liquid Web Fully Managed Web Hosting" width=120 height=240 border=0></a>
