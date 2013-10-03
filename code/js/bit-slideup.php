8225be5975702b93e9d0a3bc50af6f281ff8320c
Mika Epstein (Ipstenu) <ipstenu@ipstenu.org>
2013-10-02 20:22:34 -0700

    Mobile Detect

--- code/js/bit-slideup.php
+++ code/js/bit-slideup.php
@@ -1,63 +1,73 @@
+<?php 
+include("/home/jorjafox/public_html/content/code/mobiledetect/Mobile_Detect.php");
+
+$detect = new Mobile_Detect;
+
+if( !$detect->isMobile() && !$detect->isTablet() ){
+?>
+
     <script type="text/javascript" charset="utf-8">
-jQuery.extend(jQuery.easing,
-{
- easeOutCubic: function (x, t, b, c, d) {
-  return c*((t=t/d-1)*t*t + 1) + b;
- }
-});
-jQuery(document).ready(function($) {
-	var isopen = false,
-	    bitHeight = $('#bitsubscribe').height(),
-		$bit = $('#bit');
-	setTimeout(function () {
-		$bit.animate({
-				bottom: '-' + bitHeight - 10 + 'px'
-			}, 200);
-		if (document.location.href.indexOf('subscribe=') !== -1) {
-			open();
-		}
-	}, 300);
-	var open = function() {
-		if (isopen) return;
-		isopen = true;
-		$('a.bsub', $bit).addClass('open');
-		$('#bitsubscribe', $bit).addClass('open')
-		$bit.stop();
-		$bit.animate({
-			bottom: '0px'
-		   },{duration:400, easing:"easeOutCubic"});
-	}
-	var close = function() {
-		if (!isopen) return;
-		isopen = false;
-		$bit.stop();
-		$bit.animate({
-			bottom: '-' + bitHeight - 10 + 'px'
-		}, 200, function() { 
-			$('a.bsub', $bit).removeClass('open');
-			$('#bitsubscribe', $bit).removeClass('open');
-		});
-	}
-	$('a.bsub', $bit).click(function () {
-		if ( !isopen )
-			open();
-		else
-			close();
-	});
-	var target = $bit.has('form').length? $bit : $(document);
-	target.keyup(function(e) {
-		if (27 == e.keyCode) close();
-	});
-	
-	$( '#loggedout-follow' ).submit( function() {
-		email = $( '#loggedout-follow-field' ).val();
-		if ( '' === email || !email.match( /^.*@.*\..*$/ ) ) {
-			var error = LoggedOutFollow.invalid_email;
-			$( '#loggedout-follow-error' ).text( error ).css( 'opacity', 1 ).fadeIn( 'slow' );
-			$( '#loggedout-follow-field' ).focus( function() { $('#loggedout-follow-error').fadeOut(); } );
-			return false;
-		}
-		return true;
-	});
-});;
+        jQuery.extend(jQuery.easing,
+        {
+         easeOutCubic: function (x, t, b, c, d) {
+          return c*((t=t/d-1)*t*t + 1) + b;
+         }
+        });
+        jQuery(document).ready(function($) {
+        	var isopen = false,
+        	    bitHeight = $('#bitsubscribe').height(),
+        		$bit = $('#bit');
+        	setTimeout(function () {
+        		$bit.animate({
+        				bottom: '-' + bitHeight - 10 + 'px'
+        			}, 200);
+        		if ( (document.location.href.indexOf('subscribe=') > -1) || (document.location.href.indexOf('fb_source=') > -1) ) {
+        			open();
+        		}
+        	}, 300);
+        	var open = function() {
+        		if (isopen) return;
+        		isopen = true;
+        		$('a.bsub', $bit).addClass('open');
+        		$('#bitsubscribe', $bit).addClass('open')
+        		$bit.stop();
+        		$bit.animate({
+        			bottom: '0px'
+        		   },{duration:400, easing:"easeOutCubic"});
+        	}
+        	var close = function() {
+        		if (!isopen) return;
+        		isopen = false;
+        		$bit.stop();
+        		$bit.animate({
+        			bottom: '-' + bitHeight - 10 + 'px'
+        		}, 200, function() { 
+        			$('a.bsub', $bit).removeClass('open');
+        			$('#bitsubscribe', $bit).removeClass('open');
+        		});
+        	}
+        	$('a.bsub', $bit).click(function () {
+        		if ( !isopen )
+        			open();
+        		else
+        			close();
+        	});
+        	var target = $bit.has('form').length? $bit : $(document);
+        	target.keyup(function(e) {
+        		if (27 == e.keyCode) close();
+        	});
+        	
+        	$( '#loggedout-follow' ).submit( function() {
+        		email = $( '#loggedout-follow-field' ).val();
+        		if ( '' === email || !email.match( /^.*@.*\..*$/ ) ) {
+        			var error = LoggedOutFollow.invalid_email;
+        			$( '#loggedout-follow-error' ).text( error ).css( 'opacity', 1 ).fadeIn( 'slow' );
+        			$( '#loggedout-follow-field' ).focus( function() { $('#loggedout-follow-error').fadeOut(); } );
+        			return false;
+        		}
+        		return true;
+        	});
+        });;
     </script>
+<?php
+}
