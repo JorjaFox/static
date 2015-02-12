f220b37b450d810a157c7dd24dd5825e38506703
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-02-11 18:19:35 -0800

    Adding Site247 RUM

---
+++ code/js/rum_site247.js
@@ -0,0 +1,1 @@
+var rumMOKey='';(function(){if(window.performance && window.performance.timing && window.performance.navigation) {var site24x7_rum_beacon=document.createElement('script');site24x7_rum_beacon.setAttribute('src','//static.site24x7rum.com/js/rum-beacon-min.js');document.scripts[0].appendChild(site24x7_rum_beacon);}})(window)
