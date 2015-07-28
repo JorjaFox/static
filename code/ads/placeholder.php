dcddfe53687ec2b54d38fb72e7604ee0ddafeca6
Ipstenu <ipstenu@halfelf.org>
2015-07-28 14:25:05 -0700

    Type! No .php there

--- code/ads/placeholder.php
+++ code/ads/placeholder.php
@@ -4,9 +4,11 @@ include("/home/jorjafox/public_html/content/code/ads/projectwonderful.php");
 
 include("/home/jorjafox/public_html/content/code/ads/leaderboard-728x90.php");
 
-file_get_contents("http://jorjafox.net/content/code/ads/adboxes.php?name=halfbanner-234x60");
+$_GET['name'] = 'halfbanner-234x60';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 
-file_get_contents("http://jorjafox.net/content/code/ads/adboxes.php?name=line-buttons-500x250");
+$_GET['name'] = 'line-buttons-500x250';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 
 include("/home/jorjafox/public_html/content/code/ads/medium-rectangle-300x250.php");
 
