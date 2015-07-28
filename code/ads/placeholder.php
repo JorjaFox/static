31b3373aed35c16da3dfff6ab38cebeb0cff88e2
Ipstenu <ipstenu@halfelf.org>
2015-07-28 14:31:19 -0700

    Finally found it.

--- code/ads/placeholder.php
+++ code/ads/placeholder.php
@@ -2,7 +2,8 @@
 
 include("/home/jorjafox/public_html/content/code/ads/projectwonderful.php");
 
-include("/home/jorjafox/public_html/content/code/ads/leaderboard-728x90.php");
+$_GET['name'] = 'leaderboard-728x90';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 
 $_GET['name'] = 'halfbanner-234x60';
 include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
@@ -10,8 +11,9 @@ include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 $_GET['name'] = 'line-buttons-500x250';
 include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 
-include("/home/jorjafox/public_html/content/code/ads/medium-rectangle-300x250.php");
+$_GET['name'] = 'medium-rectangle-300x250';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 
-include("/home/jorjafox/public_html/content/code/ads/skyscraper-160x600.php");
-
+$_GET['name'] = 'skyscraper-160x600';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
 ?>
