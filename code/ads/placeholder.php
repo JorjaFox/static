be9d673d687bcccbf6167914f9ced9ecc5abd069
Ipstenu <ipstenu@halfelf.org>
2015-07-28 14:18:01 -0700

    Change to URL

--- code/ads/placeholder.php
+++ code/ads/placeholder.php
@@ -6,11 +6,11 @@ include("/home/jorjafox/public_html/content/code/ads/leaderboard-728x90.php");
 
 //include("/home/jorjafox/public_html/content/code/ads/
 
-include("/home/jorjafox/public_html/content/code/ads/adboxes.php?name=halfbanner-234x60.php");
+include("http://jorjafox.net/content/code/ads/adboxes.php?name=halfbanner-234x60.php");
 
 //include("/home/jorjafox/public_html/content/code/ads/line-buttons-500x250.php");
 
-include("/home/jorjafox/public_html/content/code/ads/adboxes.php?name=line-buttons-500x250");
+include("http://jorjafox.net/content/code/ads/adboxes.php?name=line-buttons-500x250");
 
 include("/home/jorjafox/public_html/content/code/ads/medium-rectangle-300x250.php");
 
