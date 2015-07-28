895cb60790d8c8ad6ad635ac0cd713c0287baac5
Ipstenu <ipstenu@halfelf.org>
2015-07-28 14:21:26 -0700

    Try file_get_contents

--- code/ads/placeholder.php
+++ code/ads/placeholder.php
@@ -4,13 +4,9 @@ include("/home/jorjafox/public_html/content/code/ads/projectwonderful.php");
 
 include("/home/jorjafox/public_html/content/code/ads/leaderboard-728x90.php");
 
-//include("/home/jorjafox/public_html/content/code/ads/
+file_get_contents("/home/jorjafox/public_html/content/code/ads/adboxes.php?name=halfbanner-234x60.php");
 
-include("http://jorjafox.net/content/code/ads/adboxes.php?name=halfbanner-234x60.php");
-
-//include("/home/jorjafox/public_html/content/code/ads/line-buttons-500x250.php");
-
-include("http://jorjafox.net/content/code/ads/adboxes.php?name=line-buttons-500x250");
+file_get_contents("/home/jorjafox/public_html/content/code/ads/adboxes.php?name=line-buttons-500x250");
 
 include("/home/jorjafox/public_html/content/code/ads/medium-rectangle-300x250.php");
 
