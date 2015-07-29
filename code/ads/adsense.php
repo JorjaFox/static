977775ab11fcd231b835c199ba549928b3efed8d
Ipstenu <ipstenu@halfelf.org>
2015-07-29 10:53:26 -0700

    Gogle test page

---
+++ code/ads/adsense.php
@@ -0,0 +1,15 @@
+<h1>Google Ads</h1>
+
+<?php
+
+include("/home/jorjafox/public_html/content/code/ads/loader.php");
+
+?><h2>Medium Rectangle</h2><?php
+
+$_GET['name'] = 'google-medium-rectangle';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
+
+?><h2>Responsive</h2><?php
+	
+$_GET['name'] = 'google-responsive';
+include("/home/jorjafox/public_html/content/code/ads/adboxes.php");
