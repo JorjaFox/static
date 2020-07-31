1091a6f5a84d817e5307af05e48eb9f1b2c956ae
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2020-07-31 16:29:00 -0700

    Support for Astra so I don't have to rebuild all the time.

---
+++ code/astra-addon.php
@@ -0,0 +1,11 @@
+<?php
+
+$valid_types = array( 'js', 'css' );
+
+if ( isset( $_GET['type'] ) && in_array( $_GET['type'], $valid_types ) ) {
+	foreach( glob('/home/jorjafox/jorjafox.net/static/wordpress/astra-addon/*.' . $_GET['type'] ) as $filename ){
+		echo file_get_contents( $filename );
+	}
+} else {
+	header( 'Location: https://jorjafox.net/' ) ;
+}
